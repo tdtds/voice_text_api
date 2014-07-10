14f18750e81adb0a49f24d7b6a74dedafab1f656
TADA Tadashi <t@tdtds.jp>
2014-07-10 17:31:28 +0900

    validate parameters before calling api

--- lib/voice_text_api.rb
+++ lib/voice_text_api.rb
@@ -6,16 +6,9 @@ class VoiceTextAPI
   class BadRequest < StandardError; end
   class Unauthoeized < StandardError; end
 
-  # speakers
-  SHOW   = :show
-  HARUKA = :haruka
-  HIKARI = :hikari
-  TAKERU = :takeru
-
-  # emotions
-  HAPPINESS = :happiness
-  ANGER     = :anger
-  SADNESS   = :sadness
+  ENDPOINT = URI('https://api.voicetext.jp/v1/tts')
+  SPEAKERS = %w(show haruka hikari takeru)
+  EMOTIONS = %w(happiness anger sadness)
 
   # emotion levels
   HIGHT     = 2
@@ -25,15 +18,14 @@ class VoiceTextAPI
     @api_key = api_key
   end
 
-  def tts(text, speaker = :show, emotion: nil, emotion_level: 1, pitch: 100, speed: 100, volume: 100)
+  def tts(text, speaker = :show, emotion: nil, emotion_level: NORMAL, pitch: 100, speed: 100, volume: 100)
+    validate_parameters(speaker, emotion, emotion_level, pitch, speed, volume)
+
     res = nil
-    uri = URI('https://api.voicetext.jp/v1/tts')
-    https = Net::HTTP.new(uri.host, 443)
+    https = Net::HTTP.new(ENDPOINT.host, 443)
     https.use_ssl = true
     https.start do |https|
-      req = Net::HTTP::Post.new(uri.path)
-      req.basic_auth(@api_key, '')
-      req.body = body(text, speaker, emotion, emotion_level, pitch, speed, volume)
+      req = create_request(https, text, speaker, emotion, emotion_level, pitch, speed, volume)
       res = https.request(req)
     end
 
@@ -50,14 +42,28 @@ class VoiceTextAPI
   end
 
 private
-  def body(text, speaker, emotion, emotion_level, pitch, speed, volume)
-    s = "text=#{ERB::Util.u(text)};speaker=#{ERB::Util.u(speaker)}"
+  def validate_parameters(speaker, emotion, emotion_level, pitch, speed, volume)
+    raise ArgumentError.new("wrong speaker: #{speaker}" ) unless SPEAKERS.index(speaker.to_s)
+	 if speaker != :show && emotion != nil
+    	raise ArgumentError.new("wrong emotion: #{emotion}" ) unless EMOTIONS.index(emotion.to_s)
+    end
+    raise ArgumentError.new("wrong emotion_level: #{emotion_level}" ) unless (1..2).include?(emotion_level)
+    raise ArgumentError.new("wrong pitch: #{pitch}" ) unless (50..200).include?(pitch)
+    raise ArgumentError.new("wrong speed: #{speed}" ) unless (50..200).include?(speed)
+    raise ArgumentError.new("wrong volume: #{volume}" ) unless (50..200).include?(volume)
+  end
+
+  def create_request(session, text, speaker, emotion, emotion_level, pitch, speed, volume)
+    req = Net::HTTP::Post.new(ENDPOINT.path)
+    req.basic_auth(@api_key, '')
+    s = "text=#{ERB::Util.u(text.encode('UTF-8'))};speaker=#{ERB::Util.u(speaker)}"
     s << ";emotion=#{ERB::Util.u(emotion)}" if emotion
     s << ";emotion_level=#{emotion_level.to_i}" unless emotion_level.to_i == 1
     s << ";pitch=#{pitch.to_i}" unless pitch.to_i == 100
     s << ";speed=#{speed.to_i}" unless speed.to_i == 100
     s << ";volume=#{volume.to_i}" unless volume.to_i == 100
+    req.body = s
 
-    return s
+    return req
   end
 end
